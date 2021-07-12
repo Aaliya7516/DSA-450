e710446ba319cb1897d373684b0169170728b1e9
Aaliya7516 <aaliya7516@gmail.com>
2021-07-12 19:07:12 +0530

    1

---
+++ My_Solutions/Day001Array/reverseString.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str){
+    
+  int l = str.length();
+  for (int i =0; i < l/2; i++){
+    char temp = str[i];
+    str[i] = str[l-i-1];
+    str[l-i-1] = temp;
+  }
+  cout<<str;
+}
+main(){
+    reverseWord("APFGMRZXIFPSXKOQDRRQJBBZ");
+}
