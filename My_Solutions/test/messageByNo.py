1e4ff961c9acc1ffc5632bd1fa4edf22a79c6005
Aaliya7516 <aaliya7516@gmail.com>
2021-09-10 00:22:33 +0530

    Study

---
+++ My_Solutions/test/messageByNo.py
@@ -0,0 +1,11 @@
+chat = ['Alice : Hii! How are you?', 
+"Bob : Hey! I am good, what about you?", 
+"Alice : Yeah, I am fine, I wanted your notes on Physics if it is completed.", 
+"Bob : Yeah, sure",
+"Bob : Here you go", 
+"Alice : Thanks! will return this to you by tomorrow."]
+
+userName = "Alice"
+messageNumber = 3
+
+# Find the message with the given message number from the given username in the chat.
