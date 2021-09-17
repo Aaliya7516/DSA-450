2f853eeb2aac08b4b7244e9b3cf7d515084d7fbd
Aaliya7516 <aaliya7516@gmail.com>
2021-09-18 01:22:18 +0530

    quickselect

--- My-GFG-100/003_Kth-ele-of-2-unsorted-array.py
+++ My-GFG-100/003_Kth-ele-of-2-unsorted-array.py
@@ -1,15 +1,27 @@
-import heapq
-def findKthLargest(nums, k):
-    l = len(nums)
-    heapq.heapify(nums)
-    for i in range(k):
-        heapq.heappop(nums)
+def partition(arr,l,r):
+    x = arr[r]
+    i = l
+    for j in range(l,r):
+        if arr[j] <= x:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
     
-    return heapq.heappop(nums)
+def findKthLargest(arr, l, r, k):
+    if l == r:
+        return arr[l]
+    index = partition(arr,l,r)
+    if index == k:
+        return arr[k]
+    elif index > k:
+        return findKthLargest(arr, l, index-1, k)
+    else:
+        return findKthLargest(arr, index+1, r, k-index-1)
 
 def kthElement( arr1, arr2, k):
     arr1 += arr2
-    return findKthLargest(arr1, k-1)
+    return findKthLargest(arr1, 0, len(arr1)-1, k-1)
         
 print(kthElement([2, 3, 6, 7, 9], [1, 4, 8, 10], 5))
 print(kthElement([100, 112, 256, 349, 770],[72, 86, 113, 119, 265, 445, 892], 7))
